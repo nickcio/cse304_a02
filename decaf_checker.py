2199bae2ae60452cb96b4dbbba9ae9b8f9829e64
Nick Ciotoli <98974970+nickcio@users.noreply.github.com>
2023-10-06 15:51:09 -0400

    no more debug'

--- decaf_checker.py
+++ decaf_checker.py
@@ -12,7 +12,7 @@ def just_scan():
         print("USAGE: python3 decaf_checker.py <decaf_source_file_name>")
         sys.exit()
     import decaf_lexer
-    lexer = lex.lex(module = decaf_lexer, debug = 1)
+    lexer = lex.lex(module = decaf_lexer)#, debug = 1)
 
     fh = open(fn, 'r')
     source = fh.read()
@@ -32,13 +32,13 @@ def main():
         sys.exit()
     import decaf_lexer
     import decaf_parser
-    lexer = lex.lex(module = decaf_lexer, debug = 1)
-    parser = yacc.yacc(module = decaf_parser, debug = 1)
+    lexer = lex.lex(module = decaf_lexer)#, debug = 1)
+    parser = yacc.yacc(module = decaf_parser)#, debug = 1)
 
     fh = open(fn, 'r')
     source = fh.read()
     fh.close()
-    result = parser.parse(source, lexer = lexer, debug = 1)
+    result = parser.parse(source, lexer = lexer)# debug = 1)
     #print(result)
     # Parsing Successful
     #print()
@@ -46,5 +46,5 @@ def main():
     #print()
 
 if __name__ == "__main__":
-    just_scan()
+    #just_scan()
     main()
