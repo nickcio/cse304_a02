6f0345399b465bc8078d1b112656b8fab65a5ab0
Adam Lipson <40801946+Adamlip1334@users.noreply.github.com>
2023-11-21 21:46:04 -0500

    honestly no idea what im doing

--- decaf_typecheck.py
+++ decaf_typecheck.py
@@ -3,6 +3,14 @@
 
 from decaf_ast import valid_types
 
+type_hierarchy = {
+    "Integer": "Number",
+    "Float": "Number",
+    "Number": "Object",
+    "String": "Object",
+    "Boolean": "Object",
+}
+
 def is_subtype(scope,type1,type2):
     elems = {"Integer":"int","Float":"float","String":"string","Boolean":"boolean"}
     if type1 in elems: type1=elems[type1]
@@ -30,3 +38,35 @@ def is_subclass(scope,type1,type2):
                 if type1 == type2: return True
     return False
           
+def is_number_type(var_type, scope):
+    # Convert primitive types to their corresponding object types for the hierarchy check
+    if var_type == "int":
+        var_type = "Integer"
+    elif var_type == "float":
+        var_type = "Float"
+    
+   
+    return is_subtype(scope, var_type, "Number")
+
+def is_boolean_type(var_type, scope):
+    if var_type == "boolean" or var_type == "Boolean":
+        return True
+    return is_subtype(scope, var_type, "Boolean")
+
+def resolve_boolean_expression(expression):
+    # Assuming expression is a dictionary that contains an 'operator' key.
+    operator = expression.get('operator')
+    return operator in ["&&", "||", "!", "==", "!=", "<", "<=", ">", ">="]
+
+def resolve_arithmetic_operator(operator):
+
+    arithmetic_ops = ["+", "-", "*", "/", "%", "&", "|"]
+    return operator in arithmetic_ops
+
+def resolve_arithmetic_expression(expression):
+    # Assuming expression is a dictionary that contains an 'operator' key.
+    operator = expression.get('operator')
+    return resolve_arithmetic_operator(operator)
+
+
+
